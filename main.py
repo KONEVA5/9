621c1c4aa557f0e4829037464ad2d87fef85b140
KONEVA5 <milakoneva05@gmail.com>
2024-12-07 17:26:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+number_of_homework = 12
+print(number_of_homework,type(number_of_homework))
+number_of_hours = 1.5
+print(number_of_hours,type(number_of_hours))
+course_name = "Python"
+print(course_name,type(course_name))
+time_for_one_task =print( number_of_homework / number_of_hours)
+time_for_one_task = 8
+print(time_for_one_task,type(time_for_one_task))
+print (course_name ,number_of_homework ,number_of_hours ,time_for_one_task)
